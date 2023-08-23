b09d85e3d43211f4fab24e3ca6f285852b885660
YarkaRuf <yaroslav.ershov.02@mail.ru>
2023-08-23 17:08:47 +0300

    first commit

---
+++ src/app/authentication/page.tsx
@@ -0,0 +1,13 @@
+import Pattern from 'public/svg/background/loginPattern.svg';
+
+export default function Authentication() {
+    return (
+      <main>
+        <figure>
+          <Pattern />
+        </figure>
+        aa
+      </main>
+    )
+}
+  
